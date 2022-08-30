0ed6854795820c182b54f0edbf3da21fbc19ab50
GabrielFdoVal <gabrielfdoval1@gmail.com>
2022-08-29 21:11:38 -0300

    Working on the insert_usuario.php

--- insert_usuario.php
+++ insert_usuario.php
@@ -1,12 +1,22 @@
 <?php
 
-$url = "https://192.168.15.7".$_SERVER['REQUEST_URI'];
-$components = parse_url($url);
-parse_str($components['query'], $results);
-$name = $results['name'];
-$email = $results['email'];
-$password = $results['password'];
-$birthday = $results['birthday'];
+// $url = "https://192.168.15.7".$_SERVER['REQUEST_URI'];
+// $components = parse_url($url);
+// parse_str($components['query'], $results);
+// $name = $results['name'];
+// $email = $results['email'];
+// $password = $results['password'];
+// $birthday = $results['birthday'];
+
+// $name = $_POST['name'];
+// $email = $_POST['email'];
+// $password = $_POST['password'];
+// $birthday = $_POST['birthday'];
+
+$name = "Gabriel";
+$email = "gabriel@email.com";
+$password = "123";
+$birthday = "2002-03-28";
 
 include_once("./conection.php");
 
@@ -14,10 +24,10 @@ $queryMaxCdUsuario = $pdo->query("select max(cd_usuario) from tb_usuario");
 $resMaxCdUsuario = $queryMaxCdUsuario->fetchAll(PDO::FETCH_ASSOC);
 $maxCdUsuario = json_encode($resMaxCdUsuario[0]['max(cd_usuario)']);
 $newCdUsuario = $maxCdUsuario + 1;
-
-$queryInsertUsuario = $pdo->query("insert into tb_usuario values $newCdUsuario, '$name', '$email', '$birthday', 0, 0, '$password', 'teste', 0, null");
-$resInsertUsuario = $queryInsertUsuario->fetchAll(PDO::FETCH_ASSOC);
-echo(json_encode($resInsertUsuario));
+echo($newCdUsuario);
+// $queryInsertUsuario = $pdo->query("insert into tb_usuario values $newCdUsuario, '$name', '$email', '$birthday', 0, 0, '$password', 'teste', 0, null");
+// $resInsertUsuario = $queryInsertUsuario->fetchAll(PDO::FETCH_ASSOC);
+// echo(json_encode($resInsertUsuario));
 
 
 // echo($res) ?
