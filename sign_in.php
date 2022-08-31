66da6301d21fe36f911392058ae29ac28f26622c
GabrielFdoVal <gabrielfdoval1@gmail.com>
2022-08-30 23:04:04 -0300

    Modificate conection.php  - Trying a new type of conection Modificate insert_usuario.php  - Finished the insert into tb_usuario process New file sign_in.php  - Started the signin process

--- sign_in.php
+++ sign_in.php
@@ -10,17 +10,15 @@ $link = mysqli_connect($dbhost, $dbuser, $dbpassword, $db);
 $return["error"] = true;
 $return["result"] = "Error on parameters";
 
-$val = isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["birthday"]);
+$val = isset($_POST["email"]) && isset($_POST["password"]);
 
 $val = true;
 
 if($val){
-    $name = $_POST["name"];
     $email = $_POST["email"];
     $password = $_POST["password"];
-    $birthday = $_POST["birthday"];
     
-    $sql = "SELECT MAX(cd_usuario) FROM tb_usuario";
+    $sql = "SELECT cd_usuario FROM tb_usuario WHERE cd_email = '$email' AND nm_senha = '$password'";
     $res = $link->query($sql, MYSQLI_USE_RESULT);
     
     if($res){
@@ -28,13 +26,6 @@ if($val){
         $json = json_decode($dados,true);
         $max = $json['MAX(cd_usuario)'] + 1;
 
-        $link->close();
-        $link = mysqli_connect($dbhost, $dbuser, $dbpassword, $db);
-
-        $insert_sql = "INSERT INTO tb_usuario VALUES ($max, '$name', '$email', '$birthday', 0, 0, '$password', 'teste', 0, null);";
-
-
-        $res_insert = $link->query($insert_sql, MYSQLI_USE_RESULT);
         if($res_insert){
             $return["error"] = false;
             $return["result"] = "insert sucsseful";
@@ -42,6 +33,7 @@ if($val){
             $return["error"] = true;
             $return["result"] = "Failed on insert into tb_usuario";
         }
+        
     }
     else{
         $return["error"] = true;
