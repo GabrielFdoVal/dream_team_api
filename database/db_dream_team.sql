b90fa2aa96cd713eadb8e9fb2f3a0b324b32eef8
GabrielFdoVal <gabrielfdoval1@gmail.com>
2022-11-24 22:11:44 -0300

    New Database

--- database/db_dream_team.sql
+++ database/db_dream_team.sql
@@ -1,4 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 25-Nov-2022 às 02:10
+-- Versão do servidor: 10.4.25-MariaDB
+-- versão do PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `db_dream_team`
+--
+
 DELIMITER $$
+--
+-- Procedimentos
+--
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_paga_comum` (IN `codLiga` INT(11))   begin
 	declare usu1 int(11);
 	declare usu2 int(11);
@@ -32,13 +58,26 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_paga_comum` (IN `codLiga` INT(11
 		select 'Liga ainda não acabou';
      end if;   
 end$$
+
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `log_rodada`
+--
 
 CREATE TABLE `log_rodada` (
   `cd_rodada` int(10) DEFAULT NULL,
   `cd_admin` int(10) NOT NULL,
   `dt_ajuste` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `log_tb_jogador`
+--
 
 CREATE TABLE `log_tb_jogador` (
   `cd_jogador` int(10) NOT NULL,
@@ -59,10 +98,11 @@ CREATE TABLE `log_tb_jogador` (
   `dt_ajuste` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `ordem_patrocinador_liga` (
-  `cd_patrocinador` int(10) NOT NULL,
-  `cd_liga_patrocinada` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ordem_usuario_comum`
+--
 
 CREATE TABLE `ordem_usuario_comum` (
   `cd_usuario` int(10) NOT NULL,
@@ -70,6 +110,21 @@ CREATE TABLE `ordem_usuario_comum` (
   `qt_pontos` int(3) DEFAULT NULL,
   `ic_criador` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `ordem_usuario_comum`
+--
+
+INSERT INTO `ordem_usuario_comum` (`cd_usuario`, `cd_liga_comum`, `qt_pontos`, `ic_criador`) VALUES
+(2, 1, 150, 1),
+(3, 1, 0, 0),
+(3, 2, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_admin`
+--
 
 CREATE TABLE `tb_admin` (
   `cd_admin` int(10) NOT NULL,
@@ -79,8 +134,18 @@ CREATE TABLE `tb_admin` (
   `nm_senha` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `tb_admin`
+--
+
 INSERT INTO `tb_admin` (`cd_admin`, `nm_admin`, `cd_email`, `dt_nascimento`, `nm_senha`) VALUES
-(1, 'Alison Alves', 'a.docalves@gmail.com', '2001-09-18', 'Abc123456789');
+(1, 'Alison de Oliveira', 'alison.admin@gmail.com', '2001-09-18', 'Abc12345!');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_compra_jogador`
+--
 
 CREATE TABLE `tb_compra_jogador` (
   `cd_compra` int(10) NOT NULL,
@@ -89,13 +154,19 @@ CREATE TABLE `tb_compra_jogador` (
   `dt_compra` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `tb_icone` (
-  `cd_icone` int(10) NOT NULL,
-  `vl_icone` double DEFAULT NULL,
-  `cd_url` varchar(64) DEFAULT NULL,
-  `nm_icone` varchar(45) DEFAULT NULL,
-  `nm_raridade` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--
+-- Extraindo dados da tabela `tb_compra_jogador`
+--
+
+INSERT INTO `tb_compra_jogador` (`cd_compra`, `cd_usuario`, `cd_jogador`, `dt_compra`) VALUES
+(1, 3, 1, '2022-11-25 00:39:06'),
+(2, 3, 2, '2022-11-25 00:39:09');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_jogador`
+--
 
 CREATE TABLE `tb_jogador` (
   `cd_jogador` int(10) NOT NULL,
@@ -113,22 +184,19 @@ CREATE TABLE `tb_jogador` (
   `cd_time` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `tb_jogador`
+--
+
 INSERT INTO `tb_jogador` (`cd_jogador`, `nm_jogador`, `nm_posicao`, `qt_ponto`, `qt_rebote`, `qt_toco`, `qt_bola_recuperada`, `qt_assistencia`, `qt_arremesso_errado`, `qt_turn_over`, `cd_imagem`, `qt_preco`, `cd_time`) VALUES
-(1, 'Yago', 'Armador', 27, 4, 3, 6, 10, 5, 3, 'https://lnb.com.br/wp-content/uploads/2016/10/YAGO-400x394.png', 24, 2),
-(2, 'Felipe', 'Pivô', 22, 10, 6, 4, 1, 5, 1, 'https://lnb.com.br/wp-content/uploads/2018/08/Felipe-Nascimento-1-400x400.png', 310, 2),
-(3, 'Da Silva', 'Ala', 20, 8, 2, 3, 6, 7, 3, 'https://lnb.com.br/wp-content/uploads/2020/10/TULIO-400x394.png', 23, 1),
-(4, 'Márcio', 'Ala', 19, 6, 3, 1, 2, 4, 2, 'https://lnb.com.br/wp-content/uploads/2018/08/Marcio-2-400x400.png', 16, 2),
-(5, 'Batista', 'Pivô', 12, 9, 5, 1, 0, 3, 0, 'https://lnb.com.br/wp-content/uploads/2016/10/BATISTA-400x394.png', 15, 1),
-(6, 'Du Klafke', 'Armador', 15, 4, 2, 5, 3, 3, 2, 'https://lnb.com.br/wp-content/uploads/2020/10/Du-Klafke-1-400x400.png', 15, 2),
-(7, 'Olivinha', 'Pivô', 6, 10, 5, 2, 0, 0, 1, 'https://lnb.com.br/wp-content/uploads/2016/10/OLIVINHA-1-400x381.png', 14, 1),
-(8, 'Scala', 'Armador', 14, 2, 3, 6, 7, 0, 1, 'https://lnb.com.br/wp-content/uploads/2021/07/Santiago-Scala-400x400.png', 18, 2),
-(9, 'Renato', 'Ala', 15, 4, 3, 2, 2, 0, 3, 'https://lnb.com.br/wp-content/uploads/2021/07/Renato.png', 14, 1),
-(10, 'Lucas Dias', 'Ala', 2, 12, 4, 1, 0, 3, 0, 'https://lnb.com.br/wp-content/uploads/2016/10/lucas-dias-1-400x394.png', 16, 2),
-(11, 'Teste', 'Ala', 10, 10, 10, 10, 10, 10, 10, 'https://cdn.w600.comps.canstockphoto.com.br/soldado-3d-ping%C3%BCim-banco-de-ilustra%C3%A7%C3%A3o_csp19614160.jpg', 10, 1),
-(12, 'Teste', 'Ala', 10, 10, 10, 10, 10, 10, 10, 'https://cdn.w600.comps.canstockphoto.com.br/soldado-3d-ping%C3%BCim-banco-de-ilustra%C3%A7%C3%A3o_csp19614160.jpg', 10, 1),
-(13, 'Teste', 'Ala', 10, 10, 10, 10, 10, 10, 10, 'https://cdn.w600.comps.canstockphoto.com.br/soldado-3d-ping%C3%BCim-banco-de-ilustra%C3%A7%C3%A3o_csp19614160.jpg', 10, 1),
-(14, 'Teste', 'Ala', 10, 10, 10, 10, 10, 10, 10, 'https://cdn.w600.comps.canstockphoto.com.br/soldado-3d-ping%C3%BCim-banco-de-ilustra%C3%A7%C3%A3o_csp19614160.jpg', 10, 1),
-(15, 'Teste', 'Ala', 10, 10, 10, 10, 10, 10, 10, 'https://cdn.w600.comps.canstockphoto.com.br/soldado-3d-ping%C3%BCim-banco-de-ilustra%C3%A7%C3%A3o_csp19614160.jpg', 10, 1);
+(1, 'Teste1', 'Ala', 10, 10, 10, 10, 10, 10, 10, 'https://cdn.w600.comps.canstockphoto.com.br/soldado-3d-ping%C3%BCim-banco-de-ilustra%C3%A7%C3%A3o_csp19614160.jpg', 10, 1),
+(2, 'Teste2', 'Ala', 10, 10, 10, 10, 10, 10, 10, 'https://cdn.w600.comps.canstockphoto.com.br/soldado-3d-ping%C3%BCim-banco-de-ilustra%C3%A7%C3%A3o_csp19614160.jpg', 10, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_liga_comum`
+--
 
 CREATE TABLE `tb_liga_comum` (
   `cd_liga_comum` int(10) NOT NULL,
@@ -136,28 +204,41 @@ CREATE TABLE `tb_liga_comum` (
   `qt_rodada` int(3) DEFAULT NULL,
   `qt_usuario` int(3) DEFAULT NULL,
   `ic_privada` tinyint(1) NOT NULL,
-  `cd_privada` varchar(8) NOT NULL
+  `cd_privada` varchar(8) NOT NULL,
+  `vl_entrar` int(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tb_liga_comum`
+--
+
+INSERT INTO `tb_liga_comum` (`cd_liga_comum`, `nm_liga_comum`, `qt_rodada`, `qt_usuario`, `ic_privada`, `cd_privada`, `vl_entrar`) VALUES
+(1, 'Liga comum publica 1', 12, 20, 0, '12345678', 100),
+(2, 'Liga comum criada', 6, 6, 1, '15630945', 300);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_liga_patrocinada`
+--
 
 CREATE TABLE `tb_liga_patrocinada` (
   `cd_liga_patrocinada` int(10) NOT NULL,
   `nm_liga_patrocinada` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `tb_liga_patrocinada` (`cd_liga_patrocinada`, `nm_liga_patrocinada`) VALUES
-(1, 'Liga Caixa'),
-(2, 'Liga Nike'),
-(3, 'Liga Adidas'),
-(4, 'Liga Gatorade'),
-(5, 'Liga Growth');
+--
+-- Extraindo dados da tabela `tb_liga_patrocinada`
+--
 
-CREATE TABLE `tb_ordem_compra_icone` (
-  `cd_compra` int(10) NOT NULL,
-  `cd_usuario` int(10) NOT NULL,
-  `cd_icone` int(10) NOT NULL,
-  `cd_origem` varchar(45) DEFAULT NULL,
-  `dt_compra` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `tb_liga_patrocinada` (`cd_liga_patrocinada`, `nm_liga_patrocinada`) VALUES
+(1, 'Liga Adidas');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_partida`
+--
 
 CREATE TABLE `tb_partida` (
   `cd_partida` int(10) NOT NULL,
@@ -170,14 +251,18 @@ CREATE TABLE `tb_partida` (
   `cd_rodada` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `tb_partida`
+--
+
 INSERT INTO `tb_partida` (`cd_partida`, `dt_partida`, `hr_partida`, `qt_ponto_casa`, `qt_ponto_visitante`, `cd_time_casa`, `cd_time_visitante`, `cd_rodada`) VALUES
 (1, '2022-11-16', '17:43:46', 0, 0, 1, 2, 1);
 
-CREATE TABLE `tb_patrocinador` (
-  `cd_patrocinador` int(10) NOT NULL,
-  `nm_oficial` varchar(100) DEFAULT NULL,
-  `nm_nickname` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_rodada`
+--
 
 CREATE TABLE `tb_rodada` (
   `cd_rodada` int(10) NOT NULL,
@@ -189,13 +274,29 @@ CREATE TABLE `tb_rodada` (
   `cd_turno` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `tb_rodada`
+--
+
 INSERT INTO `tb_rodada` (`cd_rodada`, `dt_inicio`, `dt_fim`, `qt_rodada`, `hr_inicio`, `hr_fim`, `cd_turno`) VALUES
 (1, '2022-11-15', '2022-11-18', 4, '18:30:04', '23:30:04', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_temporada`
+--
 
 CREATE TABLE `tb_temporada` (
   `cd_temporada` int(10) NOT NULL,
   `nm_temporada` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_time`
+--
 
 CREATE TABLE `tb_time` (
   `cd_time` int(10) NOT NULL,
@@ -203,15 +304,31 @@ CREATE TABLE `tb_time` (
   `cd_logo` varchar(120) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `tb_time`
+--
+
 INSERT INTO `tb_time` (`cd_time`, `nm_time`, `cd_logo`) VALUES
 (1, 'Flamengo', 'https://w7.pngwing.com/pngs/440/236/png-transparent-flamengo-hd-logo.png'),
 (2, 'SESI Franca', 'https://upload.wikimedia.org/wikipedia/en/7/75/Franca_basquete_logo.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_turno`
+--
 
 CREATE TABLE `tb_turno` (
   `cd_turno` int(10) NOT NULL,
   `cd_temporada` int(10) NOT NULL,
   `qt_turno` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_usuario`
+--
 
 CREATE TABLE `tb_usuario` (
   `cd_usuario` int(10) NOT NULL,
@@ -227,102 +344,139 @@ CREATE TABLE `tb_usuario` (
   `cd_liga_patrocinada` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `tb_usuario`
+--
+
 INSERT INTO `tb_usuario` (`cd_usuario`, `nm_usuario`, `cd_email`, `dt_nascimento`, `qt_leonita`, `qt_ametista`, `nm_senha`, `nm_apelido`, `qt_pontos`, `cd_tatica`, `cd_liga_patrocinada`) VALUES
-(1, 'gabriel', 'gabriel@gmail.com', '2002-03-28', 1000, 0, '1234', 'teste', 150, 0, 1),
-(2, 'gabriel', 'gabriel1@gmail.com', '2022-09-04', 550, 0, 'doVal13$$$', 'dovalzet', 200, 0, 3),
-(3, 'lucas', 'lucas@gmail.com', '2022-09-05', 0, 0, 'Lucas123$', 'luquinha', 250, 0, 3),
-(4, 'Matheus', 'matheus@email.com', '2022-09-13', 12, 0, 'matheus###1', 'matheusi', 150, 0, 3),
-(5, 'Juca', 'juca@email.com', '2022-09-25', 45, 0, 'juquinha@1', 'jucazin', 251, 0, 3),
-(6, 'Gabriel', 'lucas1@gmail.com', '2022-09-22', 0, 0, 'Lucas123%', 'luquinha', 0, 0, 3),
-(7, 'Gabriel', 'gabriel2@gmail.com', '2002-03-28', 2000, 0, 'Gabriel1$', 'gabrielz', 0, 0, 3),
-(8, 'gabriel', 'gabriel3@gmail.com', '2002-01-31', 2000, 0, 'Gabriel1@', 'Gabriel1', 0, 0, 3),
-(9, 'Gabriel', 'gabriel11111@gmai', '2002-04-16', 2000, 0, 'Gabriel1@', 'dovalzet', 0, 3, 3);
+(2, 'Jorge', 'jorge@email.com', '2001-09-18', 1000, 0, 'Jorge123!', 'jorgin', 150, 0, 1),
+(3, 'Gabriel', 'gabriel@gmail.com', '1993-11-24', 1580, 0, 'Gabriel1$$$', 'dovalz', 0, 0, 1);
 
+--
+-- Índices para tabelas despejadas
+--
 
-ALTER TABLE `ordem_patrocinador_liga`
-  ADD KEY `fk_ordem_patrocinador` (`cd_patrocinador`),
-  ADD KEY `fk_ordem_liga` (`cd_liga_patrocinada`);
-
+--
+-- Índices para tabela `ordem_usuario_comum`
+--
 ALTER TABLE `ordem_usuario_comum`
   ADD KEY `fk_ordem_comum` (`cd_liga_comum`),
   ADD KEY `fk_ordem_usuario` (`cd_usuario`);
 
+--
+-- Índices para tabela `tb_compra_jogador`
+--
 ALTER TABLE `tb_compra_jogador`
   ADD PRIMARY KEY (`cd_compra`),
   ADD KEY `fk_compra_usuario` (`cd_usuario`),
   ADD KEY `fk_compra_jogador` (`cd_jogador`);
 
-ALTER TABLE `tb_icone`
-  ADD PRIMARY KEY (`cd_icone`);
-
+--
+-- Índices para tabela `tb_jogador`
+--
 ALTER TABLE `tb_jogador`
   ADD PRIMARY KEY (`cd_jogador`),
   ADD KEY `fk_jogador_time` (`cd_time`);
 
+--
+-- Índices para tabela `tb_liga_comum`
+--
 ALTER TABLE `tb_liga_comum`
   ADD PRIMARY KEY (`cd_liga_comum`);
 
+--
+-- Índices para tabela `tb_liga_patrocinada`
+--
 ALTER TABLE `tb_liga_patrocinada`
   ADD PRIMARY KEY (`cd_liga_patrocinada`);
 
-ALTER TABLE `tb_ordem_compra_icone`
-  ADD PRIMARY KEY (`cd_compra`),
-  ADD KEY `fk_compra_icone` (`cd_usuario`);
-
+--
+-- Índices para tabela `tb_partida`
+--
 ALTER TABLE `tb_partida`
   ADD PRIMARY KEY (`cd_partida`),
   ADD KEY `fk_partida_rodada` (`cd_rodada`),
   ADD KEY `fk_partida_visitante` (`cd_time_visitante`),
   ADD KEY `fk_partida_casa` (`cd_time_casa`);
 
-ALTER TABLE `tb_patrocinador`
-  ADD PRIMARY KEY (`cd_patrocinador`);
-
+--
+-- Índices para tabela `tb_rodada`
+--
 ALTER TABLE `tb_rodada`
   ADD PRIMARY KEY (`cd_rodada`),
   ADD KEY `fk_rodada_turno` (`cd_turno`);
 
+--
+-- Índices para tabela `tb_temporada`
+--
 ALTER TABLE `tb_temporada`
   ADD PRIMARY KEY (`cd_temporada`);
 
+--
+-- Índices para tabela `tb_time`
+--
 ALTER TABLE `tb_time`
   ADD PRIMARY KEY (`cd_time`);
 
+--
+-- Índices para tabela `tb_turno`
+--
 ALTER TABLE `tb_turno`
   ADD PRIMARY KEY (`cd_turno`),
   ADD KEY `fk_turno_temporada` (`cd_temporada`);
 
+--
+-- Índices para tabela `tb_usuario`
+--
 ALTER TABLE `tb_usuario`
   ADD PRIMARY KEY (`cd_usuario`),
   ADD KEY `fk_usuario_liga` (`cd_liga_patrocinada`);
 
-ALTER TABLE `ordem_patrocinador_liga`
-  ADD CONSTRAINT `fk_ordem_liga` FOREIGN KEY (`cd_liga_patrocinada`) REFERENCES `tb_liga_patrocinada` (`cd_liga_patrocinada`),
-  ADD CONSTRAINT `fk_ordem_patrocinador` FOREIGN KEY (`cd_patrocinador`) REFERENCES `tb_patrocinador` (`cd_patrocinador`);
+--
+-- Restrições para despejos de tabelas
+--
 
+--
+-- Limitadores para a tabela `ordem_usuario_comum`
+--
 ALTER TABLE `ordem_usuario_comum`
   ADD CONSTRAINT `fk_ordem_comum` FOREIGN KEY (`cd_liga_comum`) REFERENCES `tb_liga_comum` (`cd_liga_comum`),
   ADD CONSTRAINT `fk_ordem_usuario` FOREIGN KEY (`cd_usuario`) REFERENCES `tb_usuario` (`cd_usuario`);
 
+--
+-- Limitadores para a tabela `tb_compra_jogador`
+--
 ALTER TABLE `tb_compra_jogador`
   ADD CONSTRAINT `fk_compra_jogador` FOREIGN KEY (`cd_jogador`) REFERENCES `tb_jogador` (`cd_jogador`),
   ADD CONSTRAINT `fk_compra_usuario` FOREIGN KEY (`cd_usuario`) REFERENCES `tb_usuario` (`cd_usuario`);
 
+--
+-- Limitadores para a tabela `tb_jogador`
+--
 ALTER TABLE `tb_jogador`
   ADD CONSTRAINT `fk_jogador_time` FOREIGN KEY (`cd_time`) REFERENCES `tb_time` (`cd_time`);
 
-ALTER TABLE `tb_ordem_compra_icone`
-  ADD CONSTRAINT `fk_compra_icone` FOREIGN KEY (`cd_usuario`) REFERENCES `tb_usuario` (`cd_usuario`),
-  ADD CONSTRAINT `fk_icone_usuario` FOREIGN KEY (`cd_usuario`) REFERENCES `tb_usuario` (`cd_usuario`);
-
+--
+-- Limitadores para a tabela `tb_partida`
+--
 ALTER TABLE `tb_partida`
   ADD CONSTRAINT `fk_partida_casa` FOREIGN KEY (`cd_time_casa`) REFERENCES `tb_time` (`cd_time`),
   ADD CONSTRAINT `fk_partida_rodada` FOREIGN KEY (`cd_rodada`) REFERENCES `tb_rodada` (`cd_rodada`),
   ADD CONSTRAINT `fk_partida_visitante` FOREIGN KEY (`cd_time_visitante`) REFERENCES `tb_time` (`cd_time`);
 
+--
+-- Limitadores para a tabela `tb_rodada`
+--
 ALTER TABLE `tb_rodada`
   ADD CONSTRAINT `fk_rodada_turno` FOREIGN KEY (`cd_turno`) REFERENCES `tb_turno` (`cd_turno`);
 
+--
+-- Limitadores para a tabela `tb_turno`
+--
 ALTER TABLE `tb_turno`
   ADD CONSTRAINT `fk_turno_temporada` FOREIGN KEY (`cd_temporada`) REFERENCES `tb_temporada` (`cd_temporada`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
