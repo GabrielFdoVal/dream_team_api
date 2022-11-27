205136363e558808b393f3267b5179081f8a478f
GabrielFdoVal <gabrielfdoval1@gmail.com>
2022-11-26 23:53:23 -0300

    Changed the Replace for Update

--- Repositories/Player/Update.php
+++ Repositories/Player/Update.php
@@ -1,6 +1,6 @@
 <?php
      function Update(Player $player, $cd_rodada, $cd_admin) {
-          // include($_SERVER['DOCUMENT_ROOT']."/dream_team_api/Entities/Player.php");
+          //include($_SERVER['DOCUMENT_ROOT']."/dream_team_api/Entities/Player.php");
           include($_SERVER['DOCUMENT_ROOT']."/dream_team_api/Connection.php");
           
           $DB = new DB();
@@ -45,15 +45,19 @@
                
                try {
                     $rplc = $DB->ExecSQL(
-                    "REPLACE INTO tb_jogador VALUES ($player->id, '$player->name', '$player->position', $player->point, $player->rebound, $player->block, $player->steal, $player->assist, $player->missShot, $player->turnOver, '$player->imgUrl', $points * 5, $player->team);",
+                    "UPDATE tb_jogador SET nm_jogador = '$player->name',nm_posicao = '$player->position', qt_ponto = $player->point, qt_rebote = $player->rebound, qt_toco = $player->block, qt_bola_recuperada = $player->steal, qt_assistencia = $player->assist, qt_arremesso_errado = $player->missShot, qt_turn_over = $player->turnOver, cd_imagem = '$player->imgUrl', qt_preco = $points * 5, cd_time = $player->team WHERE cd_jogador = $player->id",
                     MYSQLI_USE_RESULT
                     );
-
+                    // $rplc = $DB->ExecSQL(
+                    //      "REPLACE INTO tb_jogador VALUES (1, 'Teste1', 'Ala', 20, 10, 10, 10, 10, 10, 10, 'https://cdn.w600.comps.canstockphoto.com.br/soldado-3d-ping%C3%BCim-banco-de-ilustra%C3%A7%C3%A3o_csp19614160.jpg', 10, 1);",
+                    //      MYSQLI_USE_RESULT
+                    //      );
                     if($rplc){
                          return 204;
                     }
 
                } catch (Exception $e) {
+                    throw $e;                    
                     return 500;
                }
           } else {
